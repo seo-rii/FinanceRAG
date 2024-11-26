81f86693f81c7bd19a5f863e0b7553c0239cfd03
Seohyun Lee <me@seorii.page>
2024-11-26 22:40:06 +0900

    fix: use cuda if available

--- main.py
+++ main.py
@@ -4,23 +4,26 @@ from sentence_transformers.cross_encoder import CrossEncoder
 from financerag.tasks import FinDER
 from financerag.retrieval import SentenceTransformerEncoder, DenseRetrieval
 from financerag.rerank import CrossEncoderReranker
+import torch
 
 
 finder_task = FinDER()
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 # We need to put prefix for e5 models.
 # For more details, see Arxiv paper https://arxiv.org/abs/2212.03533
 encoder_model = SentenceTransformerEncoder(
     query_prompt='query: ',
     doc_prompt='passage: ',
-    model_name_or_path='intfloat/e5-large-v2'
+    model_name_or_path='intfloat/e5-large-v2',
+    device=device,
 )
 retriever = DenseRetrieval(model=encoder_model)
 
 # Retrieve relevant documents
 results = finder_task.retrieve(retriever=retriever)
 
-reranker = CrossEncoderReranker(CrossEncoder('cross-encoder/ms-marco-MiniLM-L-12-v2'))
+reranker = CrossEncoderReranker(CrossEncoder('cross-encoder/ms-marco-MiniLM-L-12-v2', device=device))
 reranked_results = finder_task.rerank(reranker, results, top_k=100, batch_size=32)
 
-finder_task.save_results(output_dir='res.jsonl')
+finder_task.save_results(output_dir='res')
