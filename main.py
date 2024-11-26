a46e081380f483a86a707454665398d860de4813
Seohyun Lee <me@seorii.page>
2024-11-26 22:32:11 +0900

    fix: fix main.py

--- main.py
+++ main.py
@@ -2,21 +2,18 @@
 # You can use other tasks such as `FinQA`, `TATQA`, etc.
 from financerag.common.protocols import CrossEncoder
 from financerag.tasks import FinDER
-finder_task = FinDER()
-
-from sentence_transformers import SentenceTransformer
 from financerag.retrieval import SentenceTransformerEncoder, DenseRetrieval
 from financerag.rerank import CrossEncoderReranker
 
 
-model = SentenceTransformer('intfloat/e5-large-v2')
+finder_task = FinDER()
+
 # We need to put prefix for e5 models.
 # For more details, see Arxiv paper https://arxiv.org/abs/2212.03533
 encoder_model = SentenceTransformerEncoder(
-    q_model=model,
-    doc_model=model,
     query_prompt='query: ',
-    doc_prompt='passage: '
+    doc_prompt='passage: ',
+    model_name_or_path='intfloat/e5-large-v2'
 )
 retriever = DenseRetrieval(model=encoder_model)
 
@@ -26,4 +23,4 @@ results = finder_task.retrieve(retriever=retriever)
 reranker = CrossEncoderReranker(CrossEncoder('cross-encoder/ms-marco-MiniLM-L-12-v2'))
 reranked_results = finder_task.rerank(reranker, results, top_k=100, batch_size=32)
 
-finder_task.save_results(output_dir='path_to_save_directory')
+finder_task.save_results(output_dir='res.jsonl')
